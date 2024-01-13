206697563790bd54e96e7c7082042f90aec11149
Tev Nicolas <tevnicolas@alum.calarts.edu>
2024-01-13 07:49:19 +0000

    Completed prep-javascript-functions exercise.

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,62 @@
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+returnValue = addTwoNumbers(2, 2);
+console.log('add two numbers exercise:', returnValue);
+
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+
+returnValue2 = convertHoursToMinutes(2);
+console.log('convert hours to min exercise:', returnValue2);
+
+function getGreeting(name) {
+  return 'Hello ' + name;
+}
+
+returnValue3 = getGreeting('World');
+console.log('get greeting exercise:', returnValue3);
+
+function addAndMultiplyBy5(num1, num2) {
+  return (num1 + num2) * 5;
+}
+
+returnValue4 = addAndMultiplyBy5(10, 5);
+console.log('add and multiply 5 exercise:', returnValue4);
+
+function multiplyAndDivideBy5(num1, num2) {
+  return (num1*num2) / 5;
+}
+
+returnValue5 = multiplyAndDivideBy5(35, 10);
+console.log('mult and divide 5 exercise:', returnValue5);
+
+function subtractTwoNumbers(num1, num2) {
+  return num1 - num2;
+}
+
+returnValue6 = subtractTwoNumbers(22, 7);
+console.log('subract two numbers exercise:', returnValue6);
+
+function getCircleCircumference(radius) {
+  return 2 * 3.14 * radius;
+}
+
+returnValue7 = getCircleCircumference(5);
+console.log('get circle circumference exercise:', returnValue7)
+
+function getFullName(firstName, lastName) {
+  return firstName + ' ' + lastName;
+}
+
+returnValue8 = getFullName('Juan', 'Ramirez');
+console.log('get full name exercise:', returnValue8);
+
+function cube(number) {
+  return number ** 3;
+}
+
+returnValue8 = cube(5);
+console.log('cube exercise:', returnValue8);
